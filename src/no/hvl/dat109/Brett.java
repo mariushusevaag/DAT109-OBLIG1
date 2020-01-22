ad92c823b7d0656b5c3191396d358c0b1154e1dd
GlennBarosen <glennbarosen@gmail.com>
2020-01-22 11:14:27 +0100

    lagt til brett

--- src/no/hvl/dat109/Brett.java
+++ src/no/hvl/dat109/Brett.java
@@ -1,5 +1,19 @@
 package no.hvl.dat109;
 
 public class Brett {
+	
+	private Integer[] ruter;
+	
+	public Brett(Integer[] ruter) {
+		ruter = new Integer[100];
+	}
+
+	public Integer[] getRuter() {
+		return ruter;
+	}
+
+	public void setRuter(Integer[] ruter) {
+		this.ruter = ruter;
+	}
 
 }
