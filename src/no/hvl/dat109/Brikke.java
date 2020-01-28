5223ba5d57239584b0ee47ed4956e88ecb174ed3
Charlie <charlie.nut.nut@hotmail.com>
2020-01-28 11:10:35 +0100

    Brikke klassen🇳🇴

--- src/no/hvl/dat109/Brikke.java
+++ src/no/hvl/dat109/Brikke.java
@@ -1,5 +1,45 @@
 package no.hvl.dat109;
 
 public class Brikke {
+	
+	private int nr;
+	private String farge;
+	private int pos;
+	
+
+	public int getNr() {
+		return nr;
+	}
+
+
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+
+
+	public String getFarge() {
+		return farge;
+	}
+
+
+	public void setFarge(String farge) {
+		this.farge = farge;
+	}
+
+
+	public int getPos() {
+		return pos;
+	}
+
+
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+
+
+	public Brikke(int antallBrikker) {
+		this.nr = antallBrikker + 1;
+		this.pos = 0;
+	}
 
 }
