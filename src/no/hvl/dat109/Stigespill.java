f938ef2a8b41748c032878b12a07e89fda19e3e2
mariushusevaag <marius.husevaag@local-host.no>
2020-01-31 14:40:15 +0100

    endringer i stigespill klassen🧠

--- src/no/hvl/dat109/Stigespill.java
+++ src/no/hvl/dat109/Stigespill.java
@@ -1,7 +1,11 @@
 package no.hvl.dat109;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static java.lang.Thread.sleep;
 
 import no.hvl.dat109.Brett;
 
@@ -16,8 +20,9 @@ public class Stigespill {
 	
 
 	private Brett brett;
-	private Spiller[] spillere;
-	private Spiller vinner;
+	private List<Spiller> spillere;
+	private Terning t1;
+	private boolean vunnet;
 	
 	/**
 	 * Oppretter et nytt stigespill objekt
@@ -25,14 +30,59 @@ public class Stigespill {
 	
 	public Stigespill() {
 		brett = new Brett();
-		
-		
-
-		this.vinner = null;
-		
+		spillere = new ArrayList<>();
+		t1 = new Terning();
+		vunnet = false;
 	}
 	
-	public void leggTilSpillere() {
+	public void initSpillere(ArrayList<Spiller> spillere) {
+		this.spillere = spillere;
+	}
+	
+	public Brett getBrett() {
+		return brett;
+	}
+	
+	 public void startSpill() {
+        int runder = 1;
+        System.out.println("Starter spillet...");
+
+        while (!vunnet) {
+            System.out.println("Runde: " + runder);
+            spillRunde();
+            runder++;
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        String vinner = finnVinner();
+        System.out.println("Vinneren er: " + vinner);
+
+        System.out.println("Spill avsluttet...");
+        System.exit(1);
+  	}
+	 
+	public void spillRunde() {
+		for (Spiller spiller : spillere) {
+            spiller.spillTrekk(t1);
+            if(spiller.getBrikke().getRute().getNummer() == 100){
+                vunnet=true;
+            }
+        }
+	}
+	
+	public String finnVinner() {
+		return spillere.stream()
+                .filter(s -> s.getBrikke().getRute().getNummer() == 100)
+                .map(s -> s.getNavn())
+                .collect(Collectors.joining());
+	}
+	
+	/*public void leggTilSpillere() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hvor mange skal spille?");
 		
@@ -90,5 +140,5 @@ public class Stigespill {
 				}
 			}
 		}
-	}
+	}*/
 }
