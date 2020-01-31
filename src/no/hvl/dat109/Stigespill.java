6cb287bc93fab26be3288527f1b7489af25e4436
mariushusevaag <marius.husevaag@local-host.no>
2020-01-31 12:55:23 +0100

    små endringer🐜

--- src/no/hvl/dat109/Stigespill.java
+++ src/no/hvl/dat109/Stigespill.java
@@ -26,11 +26,12 @@ public class Stigespill {
 	public Stigespill() {
 		brett = new Brett();
 		
-		leggTilSpillere();
+		
 
 		this.vinner = null;
 		
 	}
+	
 	public void leggTilSpillere() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hvor mange skal spille?");
@@ -39,34 +40,56 @@ public class Stigespill {
 		spillere = new Spiller[antSpillere];
 		
 		
-		for (Spiller s : spillere) {
-			int i = 0;
+		for (int i = 0; i < spillere.length; i++) {
 			
 			System.out.println("Skriv inn navn: ");
-			sc.next();
-			String navn = sc.nextLine();
+			String navn = sc.next();
 			
 			System.out.println("Skriv inn farge: ");
-			sc.next();
-			String farge = sc.nextLine();
+			
+			String farge = sc.next();
 			
 			Brikke b = new Brikke(i, farge);
-			s = new Spiller(navn, b);
+			Spiller s = new Spiller(navn, b);
+			spillere[i] = s;
 			
-			i++;
 		}
 		sc.close();
-	}
-
-	public void setup(Spiller[] spillere) {
-		this.spillere = spillere;
 	}
 
 	public void startSpill() {
 		Brett b = new Brett();
 		b.leggTilRutetype();
 		System.out.println(b.toString());
+		leggTilSpillere();
 
+		
+		
+		while (vinner == null) {
+			for (int i = 0; i < spillere.length && vinner == null; i++) {
+				Spiller s = spillere[i];
+				Brikke b1 = s.getBrikke();
+				Terning t = s.getTerning();
+				
+				s.spillTrekk();
+				
+				if(b1.getPos() == 99) {
+					System.out.println("Spiller: " + s.getNavn());
+					System.out.println("Kastet: " + t.getVerdi());
+					System.out.println("Pos: " + b1.getPos());
+					System.out.println("Vinneren er: " + s.getNavn());
+					vinner = s;
+				} else {
+					System.out.println("Spiller: " + s.getNavn());
+					System.out.println("Kastet: " + t.getVerdi());
+					System.out.println("Pos: " + b1.getPos());
+				}
+				
+				if(s.getNyttKast()) {
+					i--;
+				}
+			}
+		}
 	}
 	
 	
