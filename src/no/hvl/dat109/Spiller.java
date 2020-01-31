1cd7d2d13131e9dada22e55c167d88fdee0570ec
GlennBarosen <glennbarosen@gmail.com>
2020-01-31 14:43:40 +0100

    hope this works 🤞🏼

--- src/no/hvl/dat109/Spiller.java
+++ src/no/hvl/dat109/Spiller.java
@@ -7,140 +7,65 @@ package no.hvl.dat109;
  *
  */
 public class Spiller {
+	
 	private String navn;
 	private Brikke brikke;
-	private Terning t1;
-	private int sekserePaaRad;
-	private boolean trengerSekser;
-	private boolean nyttKast;
-	private Brett brett;
+	private boolean gaaTilStart;
 
-	public Spiller(String navn, Brikke brikke) {
-		this.navn = navn;
-		this.brikke = brikke;
-		this.sekserePaaRad = 0;
-		this.trengerSekser = false;
-		this.nyttKast = false;
-		this.t1 = new Terning();
-	}
-	
-	public String getNavn() {
-		return navn;
-	}
-	
-	public void setNavn(String navn) {
-		this.navn = navn;
-	}
+	public Spiller(String navn) {
+        this.navn = navn;
+        this.gaaTilStart = false;
+    }
 	
 	public Brikke getBrikke() {
 		return brikke;
 	}
-	
+
 	public void setBrikke(Brikke brikke) {
 		this.brikke = brikke;
 	}
-	
-	public Terning getTerning() {
-		return t1;
-	}
-	
-	public void setTerning(Terning t1) {
-		this.t1 = t1;
-	}
-	
-	public int getSekserePaaRad() {
-		return sekserePaaRad;
-	}
-	
-	public void setSekserePaaRad(int sekserePaaRad) {
-		this.sekserePaaRad = sekserePaaRad;
-	}
-	
-	public boolean getTrengerSekser() {
-		return trengerSekser;
-	}
-	
-	public void setTrengerSekser(boolean trengerSekser) {
-		this.trengerSekser = trengerSekser;
-	}
-	
-	public boolean getNyttKast() {
-		return nyttKast;
-	}
-	
-	public void setNyttKast(boolean nyttKast) {
-		this.nyttKast = nyttKast;
-	}
-	
-	/**
-	 * Spiller ett trekk for spilleren.
-	 */
-	public void spillTrekk() {
-		setNyttKast(false);
-		t1.trill();
-		int nyPos = brikke.getRute().getRuteNr() + t1.getVerdi();
-		
-		/**
-		 * Sjekker om spilleren trenger en sekser for å gå videre.
-		 */
-		if(getTrengerSekser()) {
-			if(t1.getVerdi() == 6) {
-				brikke.flytt(t1.getVerdi());
-				setTrengerSekser(false);
-				return;
-			} else {
-				return;
-			}
-		}
-		
-		/**
-		 * Sjekker om spilleren får en sekser.
-		 */
-		if(t1.getVerdi() == 6 && !getTrengerSekser()) {
-			setSekserePaaRad(getSekserePaaRad() + 1);
-			/**
-			 * Om det er den tredje sekseren på rad flyttes brikken til start.
-			 */
-			if(getSekserePaaRad() == 3) {
-				//Flytt til første rute + neste kast må være en sekser
-				brikke.setRute(new Rute(0));
-				setSekserePaaRad(0);
-				setTrengerSekser(true);
-				return;
-			} else {
-				//Flytt + nytt kast
-				if(nyPos < 99) {
-					brikke.flytt(t1.getVerdi());
-					setNyttKast(true);
-					//spillTrekk();
-					return;
-				} else {
-					return;
-				}
-			}
-		}
-		setSekserePaaRad(0);
-		
-		if(nyPos > 100) {
-			return;
-		}
-		
-		
-		
-		if(brikke.getRute().harSlange() || brikke.getRute().harStige()) {
-			brikke.flytt(brikke.getRute().getNyRuteNr());
-			System.out.println("Hahaha");
-		} else {
-			brikke.flytt(t1.getVerdi());
-		}
+
+	public String getNavn() {
+		return navn;
 	}
 
-	@Override
-	public String toString() {
-		return "Spiller [navn=" + navn + ", brikke=" + brikke + ", t1=" + t1 + ", sekserePaaRad=" + sekserePaaRad
-				+ ", trengerSekser=" + trengerSekser + ", nyttKast=" + nyttKast + "]";
+	public void spillTrekk(Terning terning) {
+		int seksere = 0;
+		int sum = 0;
+		
+		while (sum == 6) {
+			sum = terning.trill();
+            if (gaaTilStart && sum != 6) {
+                System.out.printf("%s trillet %d. Må trille 6 for å flytte brikken", navn, sum);
+                break;
+            }
+
+            brikke.flytt(sum);
+            System.out.printf("%s trillet %d. Flytter fra %d til %d", navn, sum, (brikke.getRute().getRuteNr() - sum), 
+            						brikke.getRute().getRuteNr());
+
+            if (brikke.getRute().harSlange()) {
+                int nyPos = brikke.getRute().getNyRuteNr();
+                System.out.printf("Woops! Du traff en slange! Du sklir ned slangen fra %d til %d",
+                					brikke.getRute().getRuteNr(), nyPos);
+                brikke.flyttTilNyPosisjon(nyPos);
+            }
+            if (brikke.getRute().harStige()) {
+                int nyPos = brikke.getRute().getNyRuteNr();
+                System.out.printf("Digg, du traff en stige! Du klatrer opp fra %d til %d",
+                					brikke.getRute().getRuteNr(), nyPos);
+                brikke.flyttTilNyPosisjon(nyPos);
+            }
+            if (sum == 6) {
+                System.out.println("Nice! Du fikk en sekser og får et nytt kast!");
+                seksere++;
+                
+                if (seksere == 3) {
+                    brikke.flyttTilNyPosisjon(1);
+                    System.out.printf("Ikke smart med tre seksere på rad! %s flyttes til start!", navn);
+                    break;
+                }
+            }
+		}
 	}
-	
-	
-	
 }
