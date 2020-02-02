c4d7aaf06e422657071ca302a71a7c295242167b
GlennBarosen <glennbarosen@gmail.com>
2020-02-02 12:47:03 +0100

    fjernet old code 🤝

--- src/no/hvl/dat109/objekter/Stigespill.java
+++ src/no/hvl/dat109/objekter/Stigespill.java
@@ -2,7 +2,6 @@ package no.hvl.dat109.objekter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import no.hvl.dat109.objekter.Brett;
@@ -82,68 +81,4 @@ public class Stigespill {
                 .collect(Collectors.joining());
 	}
 	
-	/*public void leggTilSpillere() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Hvor mange skal spille?");
-		
-		int antSpillere = sc.nextInt();
-		spillere = new Spiller[antSpillere];
-		
-		
-		for (int i = 0; i < spillere.length; i++) {
-			
-			System.out.println("Skriv inn navn: ");
-			String navn = sc.next();
-			
-			System.out.println("Skriv inn farge: ");
-			
-			String farge = sc.next();
-			
-			Brikke b = new Brikke(farge, brett);
-			Spiller s = new Spiller(navn, b);
-			spillere[i] = s;
-			
-		}
-		sc.close();
-	}
-
-	public void startSpill() {
-		String farge = "gul";
-		Brett b = new Brett();
-		Brikke brikke = new Brikke(farge, b);
-		brikke.setRute(new Rute(1));
-		brikke.flytt(1);
-		System.out.println(brikke.getRute().getRuteNr());
-		System.out.println(brikke.getRute().harStige());
-		b.leggTilRutetype();
-		leggTilSpillere();
-
-		
-		
-		while (vinner == null) {
-			for (int i = 0; i < spillere.length && vinner == null; i++) {
-				Spiller s = spillere[i];
-				Brikke b1 = s.getBrikke();
-				Terning t = s.getTerning();
-				
-				s.spillTrekk();
-				
-				if(b1.getRute().getRuteNr() == brett.STANDARD_RUTER) {
-					System.out.println("Spiller: " + s.getNavn());
-					System.out.println("Kastet: " + t.getVerdi());
-					System.out.println("Pos: " + b1.getRute().getRuteNr());
-					System.out.println("Vinneren er: " + s.getNavn());
-					vinner = s;
-				} else {
-					System.out.println("Spiller: " + s.getNavn());
-					System.out.println("Kastet: " + t.getVerdi());
-					System.out.println("Pos: " + b1.getRute().getRuteNr());
-				}
-				
-				if(s.getNyttKast()) {
-					i--;
-				}
-			}
-		}
-	}*/
 }
